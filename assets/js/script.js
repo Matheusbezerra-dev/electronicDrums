aac6151e63996fdbb6ddafd4c5bf933552ec16f8
Matheus Bezerra <mabezerra2@gmail.com>
2022-10-29 21:52:06 -0400

    feat: Algumas função foi criada
    foi feito a função de tocar usando teclas e escrever composição.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,33 @@
 "use strict";
+var _a;
+const NUMBERWAIT = 250;
 document.body.addEventListener('keyup', (e) => {
-  playsong(e.code.toLowerCase());
+    playsong(e.code.toLowerCase());
+});
+(_a = document.querySelector('.composer button')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+    let song = document.querySelector('#input');
+    if (song.value !== '') {
+        let array = song.value.split('');
+        playComposition(array);
+    }
 });
 const playsong = (song) => {
-  let songElement = document.querySelector(`#s_${song}`);
-  let keyElement = document.querySelector(`div[data-key="${song}"]`);
-  if (songElement) {
-    songElement.play();
-  }
-  if (keyElement) {
-    keyElement.classList.add('active');
-    setTimeout(() => {
-      keyElement.classList.remove('active')
-    }, 200)
-  }
+    let songElement = document.querySelector(`#s_${song}`);
+    let keyElement = document.querySelector(`div[data key="${song}"]`);
+    if (songElement) {
+        songElement.currentTime = 0;
+        songElement.play();
+    }
+    if (keyElement) {
+        keyElement.classList.add('active');
+    }
+};
+const playComposition = (array) => {
+    let wait = 0;
+    for (let itemSong of array) {
+        setTimeout(() => {
+            playsong(`key${itemSong}`);
+        }, wait);
+        wait += NUMBERWAIT;
+    }
 };
--- assets/ts/script.ts
+++ assets/ts/script.ts
@@ -1,14 +1,37 @@
+const NUMBERWAIT = 250;
+
 document.body.addEventListener('keyup', (e) => {
   playsong(e.code.toLowerCase());
 });
 
+document.querySelector('.composer button')?.addEventListener('click', () => {
+  let song: any = document.querySelector('#input');
+
+  if (song.value !== '') {
+    let array = song.value.split('');
+    playComposition(array)
+  }
+
+});
+
 const playsong = (song: any): void => {
   let songElement: any = document.querySelector(`#s_${song}`);
-  let keyElement = document.querySelector(`div[data key="${song}"]`)
+  let keyElement: any = document.querySelector(`div[data key="${song}"]`)
   if (songElement) {
+    songElement.currentTime = 0;
     songElement.play()
   } 
   if (keyElement) {
     keyElement.classList.add('active');
   }
 }; 
+
+const playComposition = (array: string[]): void => {
+  let wait = 0;
+  for(let itemSong of array) {
+    setTimeout(() => {
+      playsong(`key${itemSong}`)
+    },wait);
+    wait += NUMBERWAIT;
+  }
+}
